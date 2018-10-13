3a26d2e51d63d20be60b55b9a8a2fbda77fc7521
Sarah Pickrahn <spickrahn@gmail.com>
2018-10-12 19:45:17 -0700

    add new edits

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-README.md
+WEBPACK SETUP
 0. npm init, git init
 1. Copy the three files ont he webpack main page(src/index.js, src/bar.js, webpack.config.js)
 2. Install the webpack npm
-3. then npx webpack
+3. then install npx webpack
 4. when prompted to install webpack-cli, say y
-5.
+5.  copy final file from webpack main page
+6. confirm bundle and html and linked by opening html page in browser and checking console
+7. need babel to turn jsx into js. webpack is setup! but not react. go to babel-loader documentation. install the things in the installation list (npm instal ...)
+8. copy/paste config object into webpack config
+9. install react, react-dom, and the babel react preset `npm install react react-dom` and ??????????? ``
+10. add @babel/preset0react to our list of presets in our webpack config
+11.make an src/components/app.jsx file
+GET THE REST FROM BETH!!!!!!!!!!
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,20 +1,72 @@
 import React from 'react';
 
-class App extends React.Component {
+var movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
+
+//top level component
+var App = () => {
+    return (
+      <div>
+        <Title />
+        <Navigation movies={movies}/>
+        <List movies={movies} />
+      </div>
+    )
+};
+
+var Title = () => (
+  <h1>Movie List!</h1>
+);
+
+//this component includes the search bar and the logic assoiated with it
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isInList: false
+    };
+  }
+
+  handleSearch(props) {
+    //check if movie is in list?
+    this.props.movies.forEach((movie) => {
+      //if movie is equal to or includes the inputted text, 
+        //do this.setState = {isInList: true}
+    })
+
+  }
+
+  //after we check and see if the inputted movie is in the movie list, 
+    //we update the state and use this state to output the appropriate response
+    //and after we do this we need to return the state to false,
+    //because if we don't then all subsequent searches will return true
+
   render() {
     return (
-      <div className="shopping-list">
-        <h1>Shopping List for {this.props.name}</h1>
-        <ul>
-          <li>Twitter</li>
-          <li>WhatsApp</li>
-          <li>Oculus</li>
-        </ul>
-      </div>
-    );
+      <input type="search" id="movie-search">
+      <button>Search</button>
+    )
   }
-}
+};
 
-// Example usage: <ShoppingList name="Mark" />
+var List = (props) => {
+  const movieItems = props.movies.map((movie, key) => 
+    <ListItem key={key} value={movie} />
+  );
+
+  return (
+    <ul>{movieItems}</ul>
+  )
+};
+
+var ListItem = (props) => (
+  <li>{props.value.title}</li>
+);
+
 
 export default App;
